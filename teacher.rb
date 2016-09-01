2ca92b107650f5d0d2e8bd3dec6bf4e6b08cbc11
Chris Johnson <johnsonc.88@gmail.com>
2016-09-01 10:20:59 -0700

    undo changes from previous commit

--- teacher.rb
+++ teacher.rb
@@ -9,4 +9,24 @@ class Teacher < Employee
     super
     @target_raise = 1000
   end
+
+  def teach_stuff
+    response = ""
+    response += "Listen, class, this is how everything works, fo SHO! "
+    response += "*drops flat-out insane knowledge bomb* "
+    response += "... You're welcome. *saunters away*"
+    response
+  end
+
+  def set_performance_rating(rating)
+    response = ""
+    if rating > 90
+      receive_raise(@target_raise)
+      response = "Yay, I'm a great employee!"
+    else
+      response += "Oh, well -- thanks to this actionable, specific, and kind "
+      response += "feedback, I'll do better next time."
+    end
+    response
+  end
 end
